bc09899e8c29d188c552cde2301d9e8713129419
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2025-04-14 01:48:36 +0000

    correction de l'erreur cors

---
+++ gateway-service/src/main/java/com/silstechnologie/getwayservice/config/Config.java
@@ -0,0 +1,24 @@
+package com.silstechnologie.getwayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+@Configuration
+public class Config {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200"); // Ton front Angular
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}

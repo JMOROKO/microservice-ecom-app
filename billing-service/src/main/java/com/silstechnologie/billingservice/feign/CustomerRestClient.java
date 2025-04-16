b517202a88219f732895ad0b5437dabd8931675e
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2025-04-16 17:10:36 +0000

    correction du bug resilience4j

--- billing-service/src/main/java/com/silstechnologie/billingservice/feign/CustomerRestClient.java
+++ billing-service/src/main/java/com/silstechnologie/billingservice/feign/CustomerRestClient.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Collections;
+import java.util.List;
+
 //pour pouvoir utiliser ça il faut l'activer dans le point de demarrage de l'application
 @FeignClient(name = "customer-service")  //utilisation de openfeign pour pouvoir faire la communication entre les microservices
 public interface CustomerRestClient {
@@ -24,6 +28,19 @@ public interface CustomerRestClient {
                 .name("Default name")
                 .email("defaultEmail")
                 .build();
+    }
+
+    default PagedModel<Customer> getDefaultCustomers(Exception ex){
+        Customer customer = Customer.builder()
+                .id(1L)
+                .name("Default name")
+                .email("defaultEmail")
+                .build();
+
+        List<Customer> customers = Collections.singletonList(customer);
+
+        PagedModel.PageMetadata metadata = new PagedModel.PageMetadata(1, 0, 1, 1);
+        return PagedModel.of(customers, metadata);
     }
     /*default PagedModel<Customer> getDefaultCustomers(Long id, Exception ex){
         return Customer.builder()
